06dfb7eaf715a2a18f8f291c172ddd68df6a4825
Densitty <dennis.brough@yandex.com>
2021-05-01 01:53:07 -0500

    first project - useState hook learnt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import List from "./List";
+import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
+  const showBirthdays = () => {
+    setPeople((people) => {
+      return (people = data);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <button className="btn" onClick={() => setPeople([])}>
+            Clear All
+          </button>
+          <button className="btn" onClick={showBirthdays}>
+            Show Birthdays
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const List = (props) => {
+  console.log(props.people);
+  const { people } = props;
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article className="person" key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
