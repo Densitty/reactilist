8b1436dc2c746d6ed4ad5a9770d9a4fe1ca38007
Densitty <dennis.brough@yandex.com>
2021-05-01 03:58:39 -0500

    tours project, learnt useState, useEffect and passing props methods from top to the bottom, via props drilling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
-import React, { useState } from "react";
-import List from "./List";
-import data from "./data";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  const [people, setPeople] = useState(data);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
-  const showBirthdays = () => {
-    setPeople((people) => {
-      return (people = data);
-    });
+  // fetch the tours data
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const toursData = await response.json();
+      setLoading(false);
+      setTours(toursData);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
   };
 
-  return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <button className="btn" onClick={() => setPeople([])}>
-            Clear All
-          </button>
-          <button className="btn" onClick={showBirthdays}>
-            Show Birthdays
+  const deleteTour = (id) => {
+    const newTours = [...tours];
+    const tourIDToRemove = newTours.findIndex((tour) => {
+      return tour.id === id;
+    });
+    newTours.splice(tourIDToRemove, 1);
+    // update the state of tours
+    setTours(newTours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // if no tour has been received from the API
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // if all tours are removed
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>you have no tours</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
           </button>
         </div>
-      </section>
+      </main>
+    );
+  }
+
+  // default value returned by function
+  return (
+    <main>
+      <Tours tours={tours} removeTour={deleteTour} />
     </main>
   );
 }
