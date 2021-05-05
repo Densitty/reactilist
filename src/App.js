f913e52a6f414f88ca74cbe92d6363733e002ff5
Densitty <dennis.brough@yandex.com>
2021-05-05 08:37:18 -0500

    a grocery list, like a todo list kind

--- src/App.js
+++ src/App.js
@@ -1,55 +1,133 @@
-import React, { useState } from "react";
-import SingleColor from "./SingleColor";
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
-import Values from "values.js";
+const defaultAlert = {
+  show: false,
+  msg: "",
+  type: ``,
+};
+
+const getItemsFromLS = () => {
+  let list = localStorage.getItem("list");
+
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
 
 function App() {
-  const [color, setColor] = useState("");
-  const [error, setError] = useState(false);
-  const defaultColor = new Values("#f15025").all(10);
-  /* console.log(defaultColor); */
-  const [colorList, setColorList] = useState(defaultColor);
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getItemsFromLS);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState(defaultAlert);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // check whether value is a color or not
-    try {
-      let colors = new Values(color).all(10);
-      // console.log(colors);
-      setColorList(colors);
-    } catch (e) {
-      setError(true);
-      console.log(e.message);
+
+    if (!name) {
+      showAlert(true, "danger", "please input a value for your item");
+    } else if (name && isEditing) {
+      /* if there is a name and the btn is eidt, edit the selected item */
+
+      /* set the list to loop through the list item */
+      setList(
+        list.map((item) => {
+          // console.log(item);
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "item has been successfully edited");
+    } else {
+      /* if there is a name and the btn is submit add item to list */
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      };
+      const newList = [...list];
+      // console.log(list);
+      newList.push(newItem);
+      setList(newList);
+      showAlert(true, "success", "item has been successfully added");
+      setName("");
     }
   };
 
-  return (
-    <>
-      <section className="container">
-        <h3>color generator</h3>
-        <form action="" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={color}
-            placeholder="#f15025"
-            className={`${error ? "error" : null}`}
-            onChange={(e) => setColor(e.target.value)}
-          />
-          <button type="submit" className="btn">
-            submit
-          </button>
-        </form>
-      </section>
-      <section className="colors">
-        {colorList.map((color, index) => {
-          // console.log(index);
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
 
-          return (
-            <SingleColor key={index} {...color} index={index} hex={color.hex} />
-          );
-        })}
-      </section>
-    </>
+  const clearList = () => {
+    showAlert(true, "danger", "you now have an empty list");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed from list");
+
+    const itemIndex = list.findIndex((item) => {
+      return item.id === id;
+    });
+
+    const newList = [...list];
+    newList.splice(itemIndex, 1);
+    setList(newList);
+  };
+
+  const editItem = (id) => {
+    const item = list.find((item) => {
+      return item.id === id;
+    });
+
+    setIsEditing(true);
+    setEditID(id);
+    setName(item.title);
+    // console.log(item);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className="section-center">
+      <div className="grocery-container">
+        <form action="" className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert {...alert} alert={showAlert} list={list} />}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g tomatoes"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} removeItem={removeItem} editItem={editItem} />
+            <button className="clear-btn" onClick={clearList}>
+              clear items
+            </button>
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
 
