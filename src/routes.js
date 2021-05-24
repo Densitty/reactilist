ba1c06c7875287cf6d934e40b276fd1aa19dd117
Densitty <dennis.brough@yandex.com>
2021-05-24 16:11:15 -0500

    complete the animation of Featured

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Layout from "./HOC/Layout";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
 
 const Routes = (props) => {
-  return <Layout>Second line</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 };
 
 export default Routes;
