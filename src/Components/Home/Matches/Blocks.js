3fb171af7ce3f9fdd9723dbb89ff985206f639c1
Densitty <dennis.brough@yandex.com>
2021-05-31 04:16:32 -0500

    adding the Match blocks to display matches

--- src/Components/Home/Matches/Blocks.js
+++ src/Components/Home/Matches/Blocks.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { matchesCollection } from "../../../firebase";
 import { firebaseLooper, reverseArray } from "../../utils/miscellanous";
+import MatchesBlock from "../../utils/MatchesBlock";
+import Slide from "react-reveal/Slide";
 
 const Blocks = () => {
   const [matches, setMatches] = React.useState([]);
@@ -15,49 +17,48 @@ const Blocks = () => {
         const matches = firebaseLooper(snapshot);
         // console.log(matches);
         // matches = reverseArray(matches);
-        setMatches({
-          matches: reverseArray(matches),
-        });
+        setMatches(reverseArray(matches));
       });
-  }, [matches]);
+  }, []);
 
-  const showMatches = () => {
-    return <div>match</div>;
+  const showMatches = (matches) => {
+    console.log(matches);
+    return matches
+      ? matches.map((match) => {
+          return (
+            <Slide bottom key={match.id}>
+              <div className="item">
+                <div className="wrapper">
+                  <MatchesBlock match={match} />
+                </div>
+              </div>
+            </Slide>
+          );
+        })
+      : null;
   };
 
   return <div className="home_matches">{showMatches(matches)}</div>;
 };
 
-// class Blocks extends React.Component {
-//   state = {
-//     matches: [],
-//   };
-
-//   componentDidMount() {
-//     /* matchesCollection
-//       .limitToLast(6)
-//       .once("value")
-//       .then((snapshot) => {
-//         console.log(snapshot.val());
-//       }); */
-
-//     matchesCollection
-//       .limitToLast(6)
-//       .once("value")
-//       .then((snapshot) => {
-//         console.log(snapshot.val());
-//       });
-//   }
-
-//   showMatches = () => {
-//     return <div>match</div>;
-//   };
-
-//   render() {
-//     return (
-//       <div className="home_matches">{this.showMatches(this.state.matches)}</div>
-//     );
-//   }
-// }
+/* using the react-awesome-reveal instead of react-reveal
+import { Slide } from "react-awesome-reveal";
+const showMatches = (matches) => {
+  console.log(matches);
+  return matches
+    ? matches.map((match) => {
+        return (
+          <Slide key={match.id} className="item" triggerOnce>
+            <div>
+              <div className="wrapper">
+                <MatchesBlock match={match} />
+              </div>
+            </div>
+          </Slide>
+        );
+      })
+    : null;
+};
+*/
 
 export default Blocks;
