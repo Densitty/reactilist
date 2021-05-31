5b55212900c6e97747fb43d505f2bcb06e90f9f2
Densitty <dennis.brough@yandex.com>
2021-05-31 06:27:49 -0500

    Promotion text and image

---
+++ src/Components/Home/Promotion/Promotion.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PromotionAnimate from "./PromotionAnimate";
+
+const Promotion = () => {
+  return (
+    <div className="promotion_wrapper" style={{ background: "#fff" }}>
+      <div className="container">
+        <PromotionAnimate />
+      </div>
+    </div>
+  );
+};
+
+export default Promotion;
