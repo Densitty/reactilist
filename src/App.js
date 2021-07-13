d49e94772a52c7b43ea3a70b629617a4fdef7e59
Densitty <dennis.brough@yandex.com>
2021-07-13 05:33:46 -0500

    stripping the boilerplate to bare

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from "react";
-import "./index.css";
-import Home from "./components/Home";
-// import Game from "./components/Game";
-import Game from "./components/FunctionalGame";
-import HighScores from "./components/HighScores";
-import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div className="container">
-          <Route exact path="/" component={Home} />
-          <Route path="/game" component={Game} />
-          <Route path="/highScores" component={HighScores} />
-        </div>
-      </Router>
-    </div>
-  );
+  return <main>Hello World</main>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import Firebase from "./components/Firebase/Firebase";
-import { FirebaseContext } from "./components/Firebase/FirebaseContext";
+// import Firebase from "./components/Firebase/Firebase";
+// import { FirebaseContext } from "./components/Firebase/FirebaseContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseContext.Provider value={new Firebase()}>
-      <App />
-    </FirebaseContext.Provider>
+    {/* <FirebaseContext.Provider value={new Firebase()}> */}
+    <App />
+    {/* </FirebaseContext.Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
